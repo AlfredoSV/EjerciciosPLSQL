5950a6dd1cf4890b3c895b59d4afd51f2272537c
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-02-26 07:22:58 -0600

    Se agregan más ejercicios resueltos

--- Ejercicios.sql
+++ Ejercicios.sql
@@ -35,13 +35,61 @@ CREATE OR REPLACE PROCEDURE cad_reves (
     i       INT := length(cad);
 BEGIN
     WHILE i > 0 LOOP
-        
         cad_rev := cad_rev
                    || substr(cad, i, 1);
-                   i := i - 1;
+        i := i - 1;
     END LOOP;
 
     dbms_output.put_line(cad_rev);
 END;
 
 EXECUTE cad_reves('ALFREDO');
+
+
+
+/* Escribir una función que reciba una fecha y devuelva el año, en número, 
+correspondiente a esa fecha.
+*/
+
+CREATE OR REPLACE FUNCTION fecha_formato (
+    fecha DATE
+) RETURN INT AS
+    resultado INT := '';
+BEGIN
+    resultado := to_number(to_char(fecha, 'YY'));
+    RETURN resultado;
+END;
+
+SELECT
+    fecha_formato('30/08/2023') AS dia
+FROM
+    dual;
+
+/*Escribir un bloque PL/SQL que haga uso de la función anterior.
+*/
+
+DECLARE
+    v_anio INT := 0;
+BEGIN
+    v_anio := fecha_formato(sysdate);
+    dbms_output.put_line(v_anio);
+END;
+
+
+
+/*Desarrollar una función que devuelva el número de años completos que hay entre 
+dos fechas que se pasan como argumentos.*/
+
+
+CREATE OR REPLACE FUNCTION fecha_diferencia (
+    fecha1 DATE,
+    fecha2 DATE
+) RETURN INT AS
+    v_dif INT := 0;
+BEGIN
+    v_dif := months_between(fecha1, fecha2)/12;
+    RETURN v_dif;
+END;
+
+
+SELECT fecha_diferencia(SYSDATE,'01/10/19') FROM DUAL;
